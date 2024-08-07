5b25efa8ebb8585958055b06528b48ca76b4a042
NicolasMe9907 <mooner.nicolas999@proton.me>
2024-08-07 23:36:03 +0800

    remove ksmbd

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -462,7 +462,8 @@ define Device/cmcc_rax3000m-emmc
   DEVICE_DTS := mt7981-cmcc-rax3000m-emmc
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   SUPPORTED_DEVICES := cmcc,rax3000m-emmc
-  DEVICE_PACKAGES := $(MT7981_USB_PKGS) f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc 
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
+	luci-app-samba4
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += cmcc_rax3000m-emmc
